623bd220544bd79c39d8b7a56c603b9585e3ea1a
Bismark <bismarkpng@gmail.com>
2025-04-09 07:39:44 +0000

    feat: add "Is this really for me?" post to thoughts page

--- app/thoughts/posts/index.tsx
+++ app/thoughts/posts/index.tsx
@@ -1,8 +1,12 @@
+import * as isThisReallyForMe from "./is-this-really-for-me";
 import * as whatsThePoint from "./whats-the-point";
 import * as checkMic from "./check-mic";
 
 
 export const allPosts = [
+  {
+    ...isThisReallyForMe.metadata,
+    content: isThisReallyForMe.content, 
   {
     ...whatsThePoint.metadata,
     content: whatsThePoint.content,
