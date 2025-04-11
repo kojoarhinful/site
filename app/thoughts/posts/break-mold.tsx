7579fd63c06bca8fb126e3cf2f7679d10ba07fdd
Bismark <bismarkpng@gmail.com>
2025-04-11 06:48:36 +0000

    chore: rename post title

--- app/thoughts/posts/break-mold.tsx
+++ app/thoughts/posts/break-mold.tsx
@@ -1,11 +1,11 @@
 export const metadata = {
-  title: "Is this really for me?",
+  title: "break mold?",
   date: "Apr 09, 2025",
   readTime: "2 min read",
-  slug: "is-this-really-for-me",
+  slug: "break-mold",
 };
 
-export const content = `When I first started learning to code, I wasn’t sure if this was really for me. I remember feeling so overwhelmed like I was trying to learn a language that just didn’t click. I kept asking myself, “Is this my path? Am I even cut out for this?”
+export const content = `When I first started learning to code, I wasn’t sure if this was really for me. I felt so overwhelmed, like I was pouring effort into something that just didn’t click. I kept asking myself, 'Is this my path? Am I even cut out for this?
 
 There were days when I’d spend hours debugging a simple issue, only to realize I’d missed a semicolon or a closing bracket. It felt frustrating, and honestly, I doubted myself a lot. I’d see other developers building amazing projects, and I couldn’t help but compare myself to them. I thought maybe coding was for people who were naturally good at problem-solving, and I just wasn’t one of them.
 
