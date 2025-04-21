44134cdb73cc74cd5adbe2f2f217fa6122e4d7ea
Bismark <bismarkpng@gmail.com>
2025-04-21 10:12:09 +0000

    dynamic margin and contact link

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,16 +1,41 @@
-import { Github } from "lucide-react"
-import Link from "next/link"
+"use client";
+import { Github } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Footer() {
+  const pathname = usePathname();
+
+  const marginTopClass =
+    pathname === "/"
+      ? "mt-10 sm:mt-12 md:mt-6 lg:mt-4"
+      : "mt-20 sm:mt-48 md:mt-40 lg:mt-36";
+
   return (
-    <footer className="border-t border-amber-700/10 py-3 fixed bottom-0 left-0 right-0 bg-[#f0e9dd] bg-opacity-80 backdrop-blur-sm z-10">
-      <div className="max-w-3xl mx-auto px-4 w-full flex justify-between items-center">
-        <Link href="https://github.com/kojoarhinful/site" aria-label="GitHub" target="_blank" rel="noopener noreferrer">
-          <Github size={16} className="text-amber-800/60 hover:text-amber-900" />
-        </Link>
+    <footer
+      className={`border-t border-amber-700/10 py-3 ${marginTopClass} bg-[#f0e9dd] bg-opacity-80 backdrop-blur-sm`}
+    >
+      <div className="max-w-xl mx-auto px-3 w-full flex justify-between items-center">
+        <div className="flex items-center space-x-4">
+          <Link
+            href="https://github.com/kojoarhinful/site"
+            aria-label="GitHub"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Github
+              size={16}
+              className="text-amber-800/60 hover:text-amber-900 transition-colors duration-200"
+            />
+          </Link>
+
+          <Link href="/contact" className="group">
+            <div className="text-sm">/contact</div>
+          </Link>
+        </div>
+
         <div className="text-amber-800/60 text-xs">MMXXV</div>
       </div>
     </footer>
-  )
+  );
 }
-
