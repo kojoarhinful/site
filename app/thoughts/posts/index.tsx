b0df99da8b16f59e3454e7bfc70926576774fcab
Bismark <bismarkpng@gmail.com>
2025-04-22 07:55:51 +0000

    import procrastination

--- app/thoughts/posts/index.tsx
+++ app/thoughts/posts/index.tsx
@@ -1,13 +1,18 @@
-import * as breakMold from "./break-mold";
+import * as procrastination from "./procrastination";
+import * as isThisReallyForMe from "./is-this-really-for-me";
 import * as whatsThePoint from "./whats-the-point";
 import * as checkMic from "./check-mic";
 
 
 export const allPosts = [
+   {
+    ...procrastination.metadata,
+    content: procrastination.content,
+  },  
   {
-    ...breakMold.metadata,
-    content: breakMold.content,
-  },
+    ...isThisReallyForMe.metadata,
+    content: isThisReallyForMe.content,
+  },  
   {
     ...whatsThePoint.metadata,
     content: whatsThePoint.content,
